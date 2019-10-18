821353c226627137a3d84680b16e5d078b35497b
SaiAshish9 <saiashish3760@gmail.com>
2019-10-18 20:44:50 +0530

    Updates

---
+++ precache-manifest.5de372ab09e11d7221771e4750482bfe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71d2613cfdb25f2436296b7c5c38e286",
+    "url": "/PhotoWall/index.html"
+  },
+  {
+    "revision": "a2ff7842addae5c2cdb1",
+    "url": "/PhotoWall/static/css/main.eba1ead8.chunk.css"
+  },
+  {
+    "revision": "445ef83ff6b3760b738b",
+    "url": "/PhotoWall/static/js/2.6c7e6263.chunk.js"
+  },
+  {
+    "revision": "a2ff7842addae5c2cdb1",
+    "url": "/PhotoWall/static/js/main.8054f24b.chunk.js"
+  },
+  {
+    "revision": "bdfa9fb00f313e0a46bf",
+    "url": "/PhotoWall/static/js/runtime-main.3edb0fa3.js"
+  }
+]);
